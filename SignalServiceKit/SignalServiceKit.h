dec551193c6f84d4131117e2fcb8f13354f985a0
Igor Ranieri Elland <igor@elland.me>
2016-11-18 11:57:03 +0100

    Add carthage support.

---
+++ SignalServiceKit/SignalServiceKit.h
@@ -0,0 +1,20 @@
+//
+//  SignalServiceKit.h
+//  SignalServiceKit
+//
+//  Created by Igor Ranieri on 17/11/2016.
+//  Copyright © 2016 Whisper Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignalServiceKit.
+FOUNDATION_EXPORT double SignalServiceKitVersionNumber;
+
+//! Project version string for SignalServiceKit.
+FOUNDATION_EXPORT const unsigned char SignalServiceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalServiceKit/PublicHeader.h>
+
+
